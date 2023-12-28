0020881360bf6020eddfb7030f7b3dd95a889c43
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-28 22:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	let upp = text.toUpperCase();
+	return text + '-' + upp;
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
+   
